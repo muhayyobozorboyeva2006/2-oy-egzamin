627eed002723ebcc532018b4642f2517cba43d06
muxayyobozorboyeva1980 <muxayobozorboyeva6@gmali.com>
2025-08-12 00:59:40 +0500

    2-oyniki

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById("toggle");
+const menyular = document.getElementById("menu");
+
+toggleBtn.addEventListener("click" , function(){
+    menyular.classList.add("show");
+})
